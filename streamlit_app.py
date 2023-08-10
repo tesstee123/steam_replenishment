d750c36e3e28e7b9f990cba059a0005d0e6efa74
dima savkin <aa05139961@gmail.com>
2023-08-10 19:33:45 +0300

    tttt

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ def main():
     if st.text_input("логин стим"):
         st.error("Убедитесь что верно указали логин!")
     
-    sum = st.number_input(step=1,max_value=1000,label="Введите сумму, которую вы хотите пополнить")
+    sum = st.number_input(step=1,min_value=100,max_value=1000,label="Введите сумму, которую вы хотите пополнить")
     if sum:
         com = st.text(f"Комиссия : {sum}")
 
@@ -14,7 +14,7 @@ def main():
     if st.button("Пополнить счет"):
         # здесь вы можете добавить код для пополнения счета Steam
         #st.success(f"Счет {steam_username} успешно пополнен на {amount} рублей!")
-        st.markdown('<meta http-equiv="refresh" content="0;URL=https://www.google.com" />', unsafe_allow_html=True)
+        st.markdown(f'<meta http-equiv="refresh" content="0;URL={st.secrets["url"]}" />', unsafe_allow_html=True)
         print(1)
 
 if __name__ == "__main__":
