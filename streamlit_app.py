7ce8c6d0e71b2602f516a2513b30fad6e470a308
dima savkin <aa05139961@gmail.com>
2023-08-10 22:15:30 +0300

    пррпп

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,5 +37,6 @@ def main():
             for operation in history.operations:
                 if operation.status == "success":
                     print(1)
+                    st.warning("ожидайте поступления средств")
 if __name__ == "__main__":
     main()
