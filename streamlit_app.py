f6117ef1ef8fcd061903c120060c09776a3a3722
dima savkin <aa05139961@gmail.com>
2023-08-10 20:18:43 +0300

    hghfgcgh

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import webbrowser
 st.set_page_config(page_title="пополнение баланса стим")
 
 def main():
@@ -11,20 +11,13 @@ def main():
     if sum:
         com = st.text(f"Комиссия : {sum}")
 
-    st.write(f'''
-    <a target="_self" href="{st.secrets["url"]}">
-        <button>
-            Please login via Google
-        </button>
-    </a>
-    ''',
-    unsafe_allow_html=True
-)
-    # if st.button("Пополнить счет"):
-    #     # здесь вы можете добавить код для пополнения счета Steam
-    #     #st.success(f"Счет {steam_username} успешно пополнен на {amount} рублей!")
-    #     st.markdown(f'<meta http-equiv="refresh" content="0;URL={st.secrets["url"]}" />', unsafe_allow_html=True)
-    #     print(1)
+
+    if st.button("Пополнить счет"):
+        # здесь вы можете добавить код для пополнения счета Steam
+        #st.success(f"Счет {steam_username} успешно пополнен на {amount} рублей!")
+        webbrowser.open_new_tab("https://yoomoney.ru/quickpay/confirm.xml?receiver=4100116565978076&quickpay-form=shop&targets=Sponsor%20this%20project&paymentType=SB&sum=2&label=a1b2c3d4e6")
+        # st.markdown(f'<meta http-equiv="refresh" content="0;URL={st.secrets["url"]}" />', unsafe_allow_html=True)
+        # print(1)
 
 if __name__ == "__main__":
     main()
