7bbc6fd2f7583b53d51f5ff0feb7ba4fb1c2c21d
dima savkin <aa05139961@gmail.com>
2023-08-10 15:49:42 +0300

    test

---
+++ streamlit_app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+st.set_page_config(page_title="пополнение баланса стим")
+
+def main():
+    st.title("Пополнение счета Steam")
+    if st.text_input("логин стим"):
+        st.error("Убедитесь что верно указали логин!")
+    
+    sum = st.number_input(step=1,max_value=1000,label="Введите сумму, которую вы хотите пополнить")
+    if sum:
+        com = st.text(f"Комиссия : {sum}")
+
+
+    if st.button("Пополнить счет"):
+        # здесь вы можете добавить код для пополнения счета Steam
+        #st.success(f"Счет {steam_username} успешно пополнен на {amount} рублей!")
+        st.markdown('<meta http-equiv="refresh" content="0;URL=https://www.google.com" />', unsafe_allow_html=True)
+        print(1)
+
+if __name__ == "__main__":
+    main()
