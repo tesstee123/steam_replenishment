4be4311b493bf4acf6d5841313e3d93440d55c1b
dima savkin <aa05139961@gmail.com>
2023-08-10 21:51:36 +0300

    gdgdfg

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,40 @@
 import streamlit as st
-import webbrowser
+from yoomoney import Client
+from yoomoney import Quickpay
+import time
+token = st.secrets["ytoken"]
+client = Client(token)
+user = client.account_info()
 st.set_page_config(page_title="пополнение баланса стим")
 
 def main():
     st.title("Пополнение счета Steam")
-    if st.text_input("логин стим"):
-        st.error("Убедитесь что верно указали логин!")
-    
-    sum = st.number_input(step=1,min_value=100,max_value=1000,label="Введите сумму, которую вы хотите пополнить")
-    if sum:
-        com = st.text(f"Комиссия : {sum}")
-
+    login =  st.text_input("логин стим")
+    if login:
+        cor = st.error("Убедитесь что верно указали логин!")
+        time.sleep(3)
+        del cor
+        
+    summ = st.number_input(step=1,min_value=2,max_value=1000,label="Введите сумму, которую вы хотите пополнить")
+    if summ:
+        com = st.text(f"Комиссия : {summ}")
 
     if st.button("Пополнить счет"):
-        # здесь вы можете добавить код для пополнения счета Steam
-        #st.success(f"Счет {steam_username} успешно пополнен на {amount} рублей!")
-        st.warning("[Для оплаты нажмите на этот текст](https://yoomoney.ru/quickpay/confirm.xml?receiver=4100116565978076&quickpay-form=shop&targets=Sponsor%20this%20project&paymentType=SB&sum=2&label=a1b2c3d4e6)")
-        # st.markdown(f'<meta http-equiv="refresh" content="0;URL={st.secrets["url"]}" />', unsafe_allow_html=True)
-        # print(1)
-
+        quickpay = Quickpay(
+            receiver="4100116565978076",
+            quickpay_form="shop",
+            targets="Sponsor this project",
+            paymentType="SB",
+            sum=summ,
+            label=f"{login}"
+            )
+        print(quickpay.base_url)
+        print(quickpay.redirected_url)
+        st.warning(f"[Для оплаты нажмите на этот текст]({quickpay.redirected_url})")
+        while True:
+            history = client.operation_history(label=f"{login}")
+            for operation in history.operations:
+                if operation.status == "success":
+                    print(1)
 if __name__ == "__main__":
     main()
